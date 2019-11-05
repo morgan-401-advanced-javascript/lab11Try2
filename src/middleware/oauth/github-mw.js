07049d525c4b4965776f491c4ad62492fcd96d80
morgantatums <morgantatums@zillowgroup.com>
2019-11-04 18:51:29 -0800

    added github routes

--- src/middleware/oauth/github-mw.js
+++ src/middleware/oauth/github-mw.js
@@ -1,5 +1,32 @@
 const superagent = require('superagent');
 
-let getUserData = async request => {};
+let getUserData = async request => {
+    let authCode = request.query.code;
+
+  // TODO: Comment
+  // this uses the google token from env to authorize our application to use google OAuth 
+  let githubRes = await superagent
+    .post(process.env.GITHUB_TOKEN_SERVICE)
+    .type('form')
+    .send({
+      code: authCode,
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      redirect_uri: `${process.env.HOME_URL}/github-oauth`,
+      grant_type: 'authorization_code'
+    });
+
+  // TODO: Comment
+  let access_token = githubRes.body.access_token;
+
+  // TODO: Comment
+  githubRes = await superagent
+    .get(process.env.GITHUB_API)
+    .set('Authorization', `Bearer ${access_token}`);
+
+  // TODO: Comment
+  let userData = githubRes.body;
+  return userData;
+};
 
 module.exports = getUserData;
--- src/routes/oauth/github.js
+++ src/routes/oauth/github.js
@@ -5,8 +5,37 @@ const express = require('express');
 const githubMW = require('../../middleware/oauth/github-mw.js');
 const router = express.Router();
 
-router.get('/github', (req, res, next) => {});
+router.get('/github', (req, res, next) => {
+    let githubOAuthURL = process.env.GITHUB_AUTH_SERVICE;
+    let options = {
+      client_id: process.env.GITHUB_CLIENT_ID,
+      redirect_uri: process.env.HOME_URL + '/github-oauth',
+      scope: 'email openid profile',
+      prompt: 'consent',
+      response_type: 'code'
+    };
+  
+    // TODO: Comment
+    // adds a query to the URL
+    githubOAuthURL += '?';
+  
+    // TODO: Comment
+    Object.keys(options).forEach((key, indx) => {
+      githubOAuthURL += key + '=' + encodeURIComponent(options[key]);
+      githubOAuthURL += '&';
+    });
+  
+    // TODO: Comment
+    res.status(200).json({ url: githubOAuthURL });
 
-router.get('/github-oauth', async (req, res, next) => {});
+});
+
+router.get('/github-oauth', async (req, res, next) => {
+    let data = await githubMW(req);
+    console.log('data', data);
+
+    // TODO: Comment
+    res.status(200).json({ name: data.name, login: data.login });
+});
 
 module.exports = router;
